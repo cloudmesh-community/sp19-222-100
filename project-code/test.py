78d1f043d8a3479a2027b48afc160da3f371c3e8
mercolso <mercolso@iu.edu>
2019-04-23 23:31:00 -0400

    set up KNN testing file

--- project-code/test.py
+++ project-code/test.py
@@ -1,26 +1,23 @@
 import pandas
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-#from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix
 
 data = pandas.read_csv("data/pres.csv")
-
-print(data.head())
 
 x = data.iloc[:, 5:15].values
 y = data.iloc[:, -1].values
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.10)
-
-#scaler = StandardScaler()
-#scaler.fit(x_train)
-
-#x_train = scaler.transform(x_train)
-#x_test = scaler.transform(x_test)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
 
 classifier = KNeighborsClassifier(n_neighbors=5)
 classifier.fit(x_train, y_train)
 
 y_pred = classifier.predict(x_test)
+
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+y_pred = classifier.predict([[1,1,1,0,1,0,1,0,0,0]])
 
 print(y_pred)
